a060041da37db3bab13f5c7b4355a52b1c9729fd
macharia <mrmachariaorsam@gmail.com>
2025-03-07 14:40:30 +0300

    Updated index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 2000;
+let b = 100;
+const divide = (a, b) => a / b;
+console.log(divide(a, b));
+
+let x=2
+const square =()=>(x=x**2)
+console.log(square)
+
+let c = 3;
+let d = 4;
+const add = (a, b) => (a + b);
+console.log(c, d);
